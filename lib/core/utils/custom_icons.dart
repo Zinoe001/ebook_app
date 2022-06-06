85d4ba1cf05893f020c1b794126cf4291400b4ff
Zinoe001 <zinoetech@gmail.com>
2022-06-06 13:32:37 +0100

    Added custom Icons

---
+++ lib/core/utils/custom_icons.dart
@@ -0,0 +1,20 @@
+
+import 'package:flutter/widgets.dart';
+
+class EBookIcon {
+  EBookIcon._();
+
+  static const _kFontFam = 'EBookIcon';
+  static const String? _kFontPkg = null;
+
+  static const IconData home = IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData home_outlined = IconData(0xe801, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData bookmark = IconData(0xe802, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData search = IconData(0xe803, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData star_outlined = IconData(0xe804, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData star = IconData(0xe805, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData star_half = IconData(0xe806, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData user = IconData(0xe807, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData user_outlined = IconData(0xe808, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData bookmark_outline = IconData(0xf097, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+}
